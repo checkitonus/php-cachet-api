bb4cfa2a041cac36b0c40f2b736d8ae250f58093
Andrew Judd <awjudd@gmail.com>
2017-07-24 06:40:59 -0400

    Standardizing methods

--- src/BaseApiComponent.php
+++ src/BaseApiComponent.php
@@ -57,7 +57,7 @@ class BaseApiComponent
     {
         return $this->_server
                 ->request()
-                ->post(static::getApiRootPath(), $this->getMetadata());
+                ->post($this->getApiCreateUrl(), $this->getMetadata());
     }
 
     /**
@@ -86,7 +86,7 @@ class BaseApiComponent
 
         return $this->_server
                 ->request()
-                ->put(static::getApiRootPath() . '/' . $this['id'], $this->getMetadata());
+                ->put($this->getApiUpdateUrl($this['id']), $this->getMetadata());
     }
 
     /**
@@ -96,13 +96,88 @@ class BaseApiComponent
      */
     public function delete()
     {
+        // Is there an ID stored?
+        if(!$this['id']) {
+            // There isn't, so we need to look it up
+            $component = self::on($this->_server)
+                            ->findByName($this['name']);
+
+            // Did we get it?
+            if($component && $component['id']) {
+                // We did, so we are good
+                $this['id'] = $component['id'];
+            }
+            else {
+                // We didn't, so fail
+                return false;
+            }
+        }
+
         return $this->_server
             ->request()
-            ->delete(static::getApiRootPath() . '/' . $this['id']);
+            ->delete($this->getApiDeleteUrl());
     }
 
+    /**
+     * Retrieves the base API URL for the current object
+     *
+     * @throws     NotImplementedException  (description)
+     */
     public static function getApiRootPath()
     {
         throw new NotImplementedException();
+    }
+
+    /**
+     * The URL which will be used in order to create a new object
+     *
+     * @return     string  The object's create URL
+     */
+    public static function getApiCreateUrl()
+    {
+        return static::buildUrl(':root-path');
+    }
+
+    /**
+     * The URL which will be used in order to update a new object
+     *
+     * @return     string  The object's update URL
+     */
+    public function getApiUpdateUrl()
+    {
+        return static::buildUrl(':root-path/:id', [
+            'id' => $this['id']
+        ]);
+    }
+
+    /**
+     * The URL which will be used in order to delete a new object
+     *
+     * @return     string  The object's delete URL
+     */
+    public static function getApiDeleteUrl()
+    {
+        return static::buildUrl(':root-path/:id', [
+            'id' => $this['id']
+        ]);
+    }
+
+    /**
+     * Builds an URL with the related metadata
+     *
+     * @param      string  $url     The url
+     * @param      array   $values  The values
+     *
+     * @return     string  The url.
+     */
+    private static function buildUrl($url, array $values)
+    {
+        $values['root-path'] = static::getApiRootPath();
+
+        foreach($values as $key => $value) {
+            $url = str_replace(':' . $key, $value, $url);
+        }
+
+        return $url;
     }
 }
